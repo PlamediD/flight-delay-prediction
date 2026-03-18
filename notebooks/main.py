0a2c9814c270af032ac29959ea0f0f52620ac39a
Plamedi <pdiaku1@wgu.edu>
2026-03-17 20:50:51 -0700

    -Compute delay by day -Create visualisation for delay by day of the week -Enhance delay analysis plots with whitegrid styling and clearer day-of-week labels

--- notebooks/main.py
+++ notebooks/main.py
@@ -67,7 +67,28 @@ def plot_airline_delay(airline_delay: pd.Series, out_path: str):
     plt.show()
     print(f"Visualization saved to: {out_path}")
 
+def compute_delay_by_day(df: pd.DataFrame) -> pd.Series:
+    """Compute overall arrival delay per day."""
+    delay_by_day=(
+        df.groupby("DAY_OF_WEEK")["ARR_DEL15"].mean().sort_index()
+    )
+    print("\nDelay Rate by Day of Week:")
+    print(delay_by_day)
+    return delay_by_day
 
+def plot_delay_by_day(delay_by_day: pd.Series, out_path: str):
+    """Plot and save the delay by day."""
+    plt.figure(figsize=(8, 5))
+    sns.barplot(x=delay_by_day.index, y=delay_by_day.values)
+    plt.xticks(ticks=range(7), labels=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
+    plt.title("Delay by Day of Week")
+    plt.xlabel("Day of Week")
+    plt.ylabel("Delay Rate")
+    plt.savefig(out_path, dpi=200, bbox_inches="tight")
+    plt.show()
+    print(f"Visualization saved to: {out_path}")
+
+'''def compute_delay_by_month(df: pd.DataFrame) -> pd.Series:'''
 # -------------------------------
 # Main Workflow
 # -------------------------------
@@ -80,10 +101,18 @@ def main():
     # Compute statistics
     compute_delay_stats(df)
     airline_delay = compute_airline_delay(df)
+    delay_by_day = compute_delay_by_day(df)
+
+
 
     # Plot & save figure
+    'Arrival Delay Rate by Airline'
     plot_path = os.path.join(OUT_VIS, "arrival_delay_rate_airline.png")
     plot_airline_delay(airline_delay, plot_path)
+
+    'Delay by day of the week visualization'
+    plot_path_day = os.path.join(OUT_VIS, "delay_rate_by_day.png")
+    plot_delay_by_day(delay_by_day, plot_path_day)
 
 
 if __name__ == "__main__":
